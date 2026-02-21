b1c049d3ee9f7de45446dd2c19642d35127f4595
dawid-stolarczyk <dawsto00@gmail.com>
2026-02-21 19:47:40 +0100

    Initialize Spring Boot project with Maven, Docker Compose, and basic configuration

---
+++ src/main/java/com/github/PulsMiastaApp/PulsMiasta/PulsMiastaApplication.java
@@ -0,0 +1,13 @@
+package com.github.PulsMiastaApp.PulsMiasta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulsMiastaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PulsMiastaApplication.class, args);
+	}
+
+}
